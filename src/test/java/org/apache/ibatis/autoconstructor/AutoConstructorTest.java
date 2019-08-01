6e6270c5fbb40e0a0ef958f07945d77e14388065
huxiong <huxiong@jd.com>
2019-08-01 17:27:33 +0800

    调试环境搭建

--- src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
+++ src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
@@ -37,11 +37,13 @@ class AutoConstructorTest {
   @BeforeAll
   static void setUp() throws Exception {
     // create a SqlSessionFactory
+    // 基于mybatis-config.xml配置文件创建SqlSessionFactory
     try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/autoconstructor/mybatis-config.xml")) {
       sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
     }
 
     // populate in-memory database
+    // 基于sql文件CreateDB.sql初始化数据到内存数据库
     BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(),
         "org/apache/ibatis/autoconstructor/CreateDB.sql");
   }
