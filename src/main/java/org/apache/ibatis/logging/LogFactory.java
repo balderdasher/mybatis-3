9bfb5d942c38a99d300e970213159c60918766ce
huxiong <huxiong@jd.com>
2019-08-22 15:29:05 +0800

    日志模块

--- src/main/java/org/apache/ibatis/logging/LogFactory.java
+++ src/main/java/org/apache/ibatis/logging/LogFactory.java
@@ -18,6 +18,7 @@ package org.apache.ibatis.logging;
 import java.lang.reflect.Constructor;
 
 /**
+ * 日志工厂
  * @author Clinton Begin
  * @author Eduardo Macarron
  */
@@ -28,9 +29,13 @@ public final class LogFactory {
    */
   public static final String MARKER = "MYBATIS";
 
+  /**
+   * 使用的 Log 的构造方法
+   */
   private static Constructor<? extends Log> logConstructor;
 
   static {
+    // 逐个尝试,判断使用哪个Log实现类,即初始化 logConstructor
     tryImplementation(LogFactory::useSlf4jLogging);
     tryImplementation(LogFactory::useCommonsLogging);
     tryImplementation(LogFactory::useLog4J2Logging);
@@ -99,11 +104,14 @@ public final class LogFactory {
 
   private static void setImplementation(Class<? extends Log> implClass) {
     try {
+      // 获得参数为 String 的构造方法
       Constructor<? extends Log> candidate = implClass.getConstructor(String.class);
+      // 创建 Log 对象
       Log log = candidate.newInstance(LogFactory.class.getName());
       if (log.isDebugEnabled()) {
         log.debug("Logging initialized using '" + implClass + "' adapter.");
       }
+      // 创建成功，意味着可以使用，设置为 logConstructor
       logConstructor = candidate;
     } catch (Throwable t) {
       throw new LogException("Error setting Log implementation.  Cause: " + t, t);
